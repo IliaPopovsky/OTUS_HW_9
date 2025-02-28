85c68e632174f300393ace9240f64a83dcbba037
Ilia Popovsky <popovskyilia@gmail.com>
2025-02-28 20:26:12 +0300

    first version project1

---
+++ logger.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <assert.h>
+#include <execinfo.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include "logger.h"
