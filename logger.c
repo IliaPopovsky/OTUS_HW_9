88fe46292a4a9914070810389e2b2c9c2b46aa19
Ilia Popovsky <popovskyilia@gmail.com>
2025-03-02 19:50:40 +0300

    first version project2

--- logger.c
+++ logger.c
@@ -8,3 +8,12 @@
 #include <unistd.h>
 #include <sys/syscall.h>
 #include "logger.h"
+
+#define MAX_LEN 255
+#define SIZE_BUF 100
+
+static struct {
+    FILE* output;
+    char filename[MAX_LEN + 1];
+} log_t;
+
