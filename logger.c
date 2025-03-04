f6885562694e6e092186becc4769d2b849550ee5
Ilia Popovsky <popovskyilia@gmail.com>
2025-03-04 13:42:52 +0300

    first version project3

--- logger.c
+++ logger.c
@@ -12,8 +12,128 @@
 #define MAX_LEN 255
 #define SIZE_BUF 100
 
+static volatile int logger_activate;
+static volatile LogLevel logLevel_t = LogLevel_INFO;
+static pthread_mutex_t mutex;
+
 static struct {
     FILE* output;
     char filename[MAX_LEN + 1];
 } log_t;
 
+
+void logger_setLevel(LogLevel level)
+{
+    logLevel_t = level;
+}
+
+LogLevel logger_getLevel(void)
+{
+    return logLevel_t;
+}
+
+int logger_levelenough(LogLevel level)
+{
+    return logLevel_t <= level;
+}
+
+static char * getLevel(LogLevel level)
+{
+    switch (level) {
+        case LogLevel_DEBUG: return "DEBUG";
+        case LogLevel_INFO:  return "INFO";
+        case LogLevel_WARN:  return "WARN";
+        case LogLevel_ERROR: return "ERROR";
+        default: return "";
+    }
+}
+
+int logger_init(const char * filename){
+    if (logger_activate) {
+        fprintf(stderr, "logger allready initializated!");
+        return 1;
+    }
+    if (filename == NULL) {
+        assert(0 && "filename must not be NULL");
+        return 0;
+    }
+    if (strlen(filename) > MAX_LEN) {
+        assert(0 && "filename exceeds the maximum number of characters");
+        return 0;
+    }
+    pthread_mutex_init(&mutex, NULL);
+    pthread_mutex_lock(&mutex);
+    if (log_t.output != NULL) {
+        fclose(log_t.output);
+    }
+    log_t.output = fopen(filename, "a");
+    if (log_t.output == NULL) {
+        fprintf(stderr, "ERROR: logger: Failed to open file: `%s`\n", filename);
+        pthread_mutex_unlock(&mutex);
+        return 0;
+    }
+    strncpy(log_t.filename, filename, sizeof(filename) + 1);
+
+    logger_activate = 1;
+    pthread_mutex_unlock(&mutex);
+    return 1;
+}
+
+static void logger_stacktraceprint(FILE* fp){
+    int nptrs;
+    void *buffer[SIZE_BUF];
+    char **strings;
+    nptrs = backtrace(buffer, SIZE_BUF);
+    fprintf(fp, "\tbacktrace() returned %d addresses\n", nptrs);
+    strings = backtrace_symbols(buffer, nptrs);
+    if (strings == NULL) {
+        perror("backtrace_symbols");
+        return;
+    }
+    for (int j = 0; j < nptrs; j++)
+        fprintf(fp, "\t%s\n", strings[j]);
+    free(strings);
+}
+
+static void write_log(FILE* fp, char * levelstr, int threadID,
+        const char* file, int line, const char* fmt, va_list arg){
+    fprintf(fp, "%s: threadId:%d %s:%d: ", levelstr, threadID, file, line);
+    vfprintf(fp, fmt, arg);
+    fprintf(fp, "\n");
+    if (!strcmp(levelstr, "ERROR")){
+        logger_stacktraceprint(fp);
+    }
+    return;
+}
+
+void logger_log(LogLevel level, const char* file, int line, const char* fmt, ...){
+
+    char * levelstr;
+    va_list arg;
+    int threadID;
+
+    if (!logger_activate) {
+        assert(0 && "logger is not initialized");
+        return;
+    }
+
+    if (!logger_levelenough(level)) {
+        return;
+    }
+    threadID = syscall(SYS_gettid);
+    pthread_mutex_lock(&mutex);
+    levelstr = getLevel(level);
+
+    va_start(arg, fmt);
+    write_log(log_t.output, levelstr, threadID, file, line, fmt, arg);
+    va_end(arg);
+
+    pthread_mutex_unlock(&mutex);
+}
+
+void logger_exit(){
+    pthread_mutex_destroy(&mutex);
+    if (log_t.output) {
+        fclose(log_t.output);
+    }
+}
--- main.c
+++ main.c
@@ -1,8 +1,53 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include "logger.h"
 
-int main()
+void* thread_body(void* arg)
 {
-    printf("Hello world!\n");
+    for(int i = 0; i < 2; ++i)
+    {
+        puts(arg);
+        char *test_block = NULL;
+        sleep(1);
+        test_block = malloc(10000000000 * 10000000000);
+        if (test_block == NULL){
+            LOG_ERROR("malloc test_block error!");
+        }
+        sleep(2);
+    }
+    return NULL;
+}
+
+int main(void)
+{
+    logger_init("logfile.txt");
+    LogLevel level = logger_getLevel();
+    if (level < LogLevel_INFO) {
+        printf("$$$$$$");
+    }
+    logger_setLevel(LogLevel_DEBUG);
+    char *test_block_main = NULL;
+    test_block_main = malloc(10000000000 * 10000000000);
+    if (test_block_main == NULL){
+        LOG_ERROR("malloc test_block_main error!");
+    }
+    pthread_t thread1, thread2;
+    pthread_create(&thread1, NULL, thread_body, "Test string1");
+    pthread_create(&thread2, NULL, thread_body, "Test string2");
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    printf("End test.\n");
+    char *test_string = "Ffd";
+    LOG_INFO("console %s logging", "test");
+    int l = logger_getLevel();
+    LOG_DEBUG("format example: %d%c%s", 1, '2', "3");
+    LogLevel f = LogLevel_WARN;
+    printf("Test print %p\n", test_string);
+    printf("f: %d\n", f);
+    printf("l: %d\n", l);
     return 0;
 }
